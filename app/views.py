60e8765ea48ba29b0850e1513f740a067bb70998
azimabdurasulov <azimjon20050604@gmail.com>
2023-03-02 10:33:18 +0500

    init

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse,HttpRequest,JsonResponse
+import json
 def index(request:HttpRequest):
     a = request.GET.get('a', 0)
     b = request.GET.get('b', 0)
     return JsonResponse({
         'result':int(a)+int(b)
+    })
+
+def get_data(request: HttpRequest):
+    if request.method == 'GET':
+        print('GET')
+
+    if request.method == 'POST':
+        data = request.body.decode()
+        data_a = json.loads(data)
+        a = data_a['a']
+        b = data_a['b']
+
+    return JsonResponse({
+        'result': int(a) + int(b)
     })
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import index
+from app.views import index, get_data
 
 urlpatterns = [
     path('', index),
+    path('get_data/', get_data)
 ]
